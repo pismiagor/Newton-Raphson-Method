818dfa57a8df1845d90dcce24c16b6f34774492b
Emre Can <124934936+pismiagor@users.noreply.github.com>
2023-03-21 17:01:46 +0300

    Add files via upload

---
+++ Newton-Raphson Method with Constant Slope.py
@@ -0,0 +1,37 @@
+def function(y: str, variable: str, x):
+
+    equation = ""
+
+    for i in y:
+        if i != variable:
+            equation += i
+        else:
+            equation += "x"
+
+    return eval(equation)
+
+def takeDerivative(y: str, variable: str, x, h):
+
+    equationPlush = ""
+
+    for i in y:
+        if i != variable:
+            equationPlush += i
+        else:
+            equationPlush += "(x+h)"
+
+    func = function(y, variable, x)
+
+    derivative = ( eval(equationPlush) - func ) / h
+
+    return derivative
+
+def newtonRaphson(y: str, variable: str, x, h, num_iterations):
+
+    constantSlope = takeDerivative(y, variable, x, h)
+
+    for j in range(num_iterations): 
+        
+        x -= function(y, variable, x) / constantSlope
+
+    return x
---
+++ Newton-Raphson Method with Tolerance Interval.py
@@ -0,0 +1,39 @@
+def function(y: str, variable: str, x):
+
+    equation = ""
+
+    for i in y:
+        if i != variable:
+            equation += i
+        else:
+            equation += "x"
+
+    return eval(equation)
+
+def takeDerivative(y: str, variable: str, x, h):
+
+    equationPlush = ""
+
+    for i in y:
+        if i != variable:
+            equationPlush += i
+        else:
+            equationPlush += "(x+h)"
+
+    func = function(y, variable, x)
+
+    derivative = ( eval(equationPlush) - func ) / h
+
+    return derivative
+
+def newtonRaphson(y: str, variable: str, x, h, tolerance):
+
+    firstStep = function(y, variable, x) / takeDerivative(y, variable, x, h)
+
+    while abs(firstStep) > tolerance: 
+        
+        x -= function(y, variable, x) / takeDerivative(y, variable, x, h)
+
+        firstStep = function(y, variable, x) / takeDerivative(y, variable, x, h)
+
+    return x
